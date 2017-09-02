28ebec7811344a080cd3c1a82f1adf09432591fa
ReeceLangerock <ReeceLangerock@gmail.com>
2017-09-01 21:38:06 -0500

    add leaderboardheader component

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,10 +1,35 @@
-import React from 'react'
+import React from "react";
+import LeaderboardHeader from "./LeaderboardHeader";
+import LeaderboardRow from "./LeaderboardRow";
+import { connect } from 'react-redux'
+
+import * as actions from '../actions/actions'
 
 export class Leaderboard extends React.Component {
-  render () {
+  constructor(props) {
+    super(props);
+  }
+
+  componentWillMount() {}
+
+  renderLeaderboardRow() {
+
+    return <LeaderboardRow />;
+  }
+  render() {
     return (
-      <div className='Leaderboard' />
-    )
+      <div className="Leaderboard">
+        <h1>{this.props.selection}</h1>
+        <LeaderboardHeader />
+        {this.renderLeaderboardRows}
+      </div>
+    );
   }
 }
-export default Leaderboard
+
+const mapStateToProps = (state) => ({
+  selection: state.camperReducer.selection,
+  recentCampers: state.camperReducer.recentCampers,
+  allTimeCampers: state.camperReducer.allTimeCampers
+})
+export default connect(mapStateToProps, actions)(Leaderboard)
---
+++ src/components/LeaderboardHeader.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { connect } from 'react-redux'
+import * as actions from '../actions/actions'
+
+export class LeaderboardHeader extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e){
+    console.log()
+    this.props.setSelection(e.target.value)
+  }
+
+  render() {
+    return <div className="LeaderboardHeader">
+      <button value = 'recent' onClick={this.handleClick}>Recent</button>
+      <button value = 'alltime' onClick={this.handleClick}>All-Time</button>
+      </div>;
+  }
+}
+
+const mapStateToProps = () => ({
+
+})
+export default connect(mapStateToProps, actions)(LeaderboardHeader)
